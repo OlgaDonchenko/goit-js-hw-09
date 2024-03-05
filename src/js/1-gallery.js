6e3c97c38082a03ecc143cd038ec6a25603a7cea
OlgaDonchenko <n0931606045@gmail.com>
2024-03-05 13:35:43 +0200

    fixxxxxx

--- src/js/1-gallery.js
+++ src/js/1-gallery.js
@@ -1,7 +1,7 @@
 
 
-import SimpleLightbox from "simplelightbox/dist/simple-lightbox.esm"
-import "simplelightbox/dist/simple-lightbox.min.css";
+import SimpleLightbox from "simplelightbox"
+import "simplelightbox/dist/simple-lightbox.css";
 
 const images = [
   {
